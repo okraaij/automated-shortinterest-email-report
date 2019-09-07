469502748f6759e2c0cb7faa9aac40d6bef1fbbc
oliverk1 <44778068+oliverk1@users.noreply.github.com>
2019-09-07 21:18:23 +0200

    Update si.py

--- si.py
+++ si.py
@@ -18,8 +18,8 @@ from email import encoders
 # Connect to database and select data from the past 16 weeks
 conn = pyodbc.connect(
     "Driver={SQL Server Native Client 11.0};"
-    "Server={PWSQL210};"
-    "Database=TradingDB;"
+    "Server={***};"
+    "Database=***;"
     "Trusted_Connection=yes;"
 )
 
@@ -55,7 +55,7 @@ def runscript(datapoints):
     new = new[['Id','TickerId','Value','Date','UpdateTimeStamp','DataType','IsPercentage','Ticker']]
 
     # Add full names to dataframe
-    fullname = pd.read_csv("C:/Users/Olivier.Kraaijeveld/Documents/Projecten/Project Short/Sourcefiles/fullname.csv", sep=";", names=['Ticker', 'FullName'], skiprows=[0])
+    fullname = pd.read_csv("fullname.csv", sep=";", names=['Ticker', 'FullName'], skiprows=[0])
     new = pd.merge(new, fullname, how='left', on='Ticker')
 
     # Apply pivot table to obtain table per date
@@ -142,8 +142,6 @@ def runscript(datapoints):
         names = []
         percs = []
         latestsi = []
-
-        # Store tickers that had no data
         no_data = []
 
         for item in lastweekdatedf.Ticker:
@@ -201,31 +199,33 @@ def runscript(datapoints):
     #  Compose email  #
     ###################
     
-    sender_email = "olivier.kraaijeveld@kempen.com"
-    receiver_email = ['pepijn.kluin@kempen.com', 'martin.ligthart@kempen.com', 'huib.meulenbelt@kempen.com', 'olivier.kraaijeveld@kempen.com']
+    sender_email = "example@example.com"
+    receiver_email = ['example@example.com']
 
     message = MIMEMultipart("alternative")
     message["Subject"] = "Weekly short interest report"
-    message["From"] = "Kempen Securities"
+    message["From"] = "Sender name you want to appear in email header"
     message["To"] = ", ".join(receiver_email)
 
     html = """\
     <html>
-    <p>Dear colleague,</p><p>Please find the highlights of this week's automated short interest report below and the complete report in the attachment.</p><p>The data has been calculated between <em>""" + str(lastweekdate.strftime('%A %d %B %Y')) + """</em> and <em>""" + str(latestdate.strftime('%A %d %B %Y')) + """</em> (data has a 1-day delay).</p><p>Please note that if no data was present for one week ago, the tool will find the nearest historical date that has data available.</p><h4>Top 15 shorts % of free float</h4><p>""" + str(topshorts()) + """</p><h4>Top 15 shorts days to cover</h4><p>""" + str(daystocover()) + """</p><h4>Top 15 weekly increases</h4><p>""" + str(percentages()[0]) + """</p><h4>Top 15 weekly decreases</h4><p>""" + str(percentages()[1]) + """</p><h4>Excluded data</h4><p>For the following companies either missing and/or incorrect data was found: &nbsp;</p> <p><em>""" + percentages()[2] + """</em> &nbsp;</p></p><p>The above tickers have therefore <strong>not</strong> been included in the short report! Please check the data if a company's name returns periodically.<p><p>If you have any questions and/or remarks, please contact Pepijn Kluin <a href='mailto:Pepijn.Kluin@kempen.com'>here</a>.</p><p>Kind regards,</p><p>Kempen Securities</p><p><span style='font-size: 6.0pt; text-transform: uppercase; letter-spacing: .5pt;'>Kempen&nbsp; N.V.&nbsp; Beethovenstraat 300&nbsp;&nbsp; 1077 WZ Amsterdam <br />Chamber of Commerce Amsterdam: 34186722</span><span style='font-size: 7.0pt; text-transform: uppercase; letter-spacing: .5pt;'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></p><p><span style='font-size: 7.0pt; text-transform: uppercase; letter-spacing: .5pt;'>THIS IS AN AUTOMATED MESSAGE, PLEASE DO NOT REPLY TO THIS EMAIL ADDRESS!<br /><br /></span></p><p><span style='font-size: 7.0pt; text-transform: uppercase; letter-spacing: .5pt;'>end of message</span></p>
+    <p>Dear colleague,</p> <p>Please find the highlights of this week's automated short interest report below and the complete report in the attachment.</p> <p>The data has been calculated between <em>""" + str(lastweekdate.strftime('%A %d %B %Y')) + """</em> and <em>""" + str(latestdate.strftime('%A %d %B %Y')) + """</em> (data has a 1-day delay).</p> <p>Please note that if no data was present for one week ago, the tool will find the nearest historical date that has data available.</p> <h4>Top 15 shorts % of free float</h4> <p>""" + str(topshorts()) + """</p> <h4>Top 15 shorts days to cover</h4> <p>""" + str(daystocover()) + """</p> <h4>Top 15 weekly increases</h4> <p>""" + str(percentages()[0]) + """</p> <h4>Top 15 weekly decreases</h4> <p>""" + str(percentages()[1]) + """</p> <h4>Excluded data</h4> <p>For the following companies either missing and/or incorrect data was found: &nbsp;</p> <p><em>""" + percentages()[2] + """</em> &nbsp;</p> <p>The above tickers have therefore <strong>not</strong> been included in the short report! Please check the data if a company's name returns periodically.</p> <p>If you have any questions and/or remarks, please contact XXX <a href="mailto:example@example.com">here</a>.</p> <p>Kind regards,</p> <p>Name<span style="font-size: 7.0pt; text-transform: uppercase; letter-spacing: .5pt;">&nbsp; &nbsp;</span></p> <p><span style="font-size: 7.0pt; text-transform: uppercase; letter-spacing: .5pt;">THIS IS AN AUTOMATED MESSAGE, PLEASE DO NOT REPLY TO THIS EMAIL ADDRESS!<br /><br /></span></p>
     </html>
     """
     
     message.attach(MIMEText(html, "html"))
     
+    # Include attachment
     part = MIMEBase('application', "octet-stream")
-    part.set_payload(open("K:\Trading\Short Interest Report\Short Interest Report.xlsx", "rb").read())
+    part.set_payload(open("attachment.xlsx", "rb").read())
     encoders.encode_base64(part)
-    part.add_header('Content-Disposition', 'attachment; filename="Short Interest Report.xlsx"')
+    part.add_header('Content-Disposition', 'attachment; filename="attachment.xlsx"')
 
     message.attach(part)
 
-    server = smtplib.SMTP('smtp.vlkintern.nl', 25)
-    server.connect("smtp.vlkintern.nl", 25)
+    # Enter server details
+    server = smtplib.SMTP('smtp.servername.nl', 25)
+    server.connect("smtp.servername.nl", 25)
     server.sendmail(sender_email, receiver_email, message.as_string())
     server.quit()
 
@@ -245,23 +245,23 @@ if datapoints.UpdateTimeStamp.iloc[0].date() == date.today() and date.today().st
 if not datapoints.UpdateTimeStamp.iloc[0].date() == date.today():
     
     # Send email to notify data has to be updated   
-    sender_email = "olivier.kraaijeveld@kempen.com"
-    receiver_email = ['pepijn.kluin@kempen.com', 'olivier.kraaijeveld@kempen.com']
+    sender_email = "example@example.com"
+    receiver_email = ['example@example.com']
 
     message = MIMEMultipart("alternative")
     message["Subject"] = "Please update the short interest information!"
-    message["From"] = "Kempen Securities"
+    message["From"] = "Sender name you want to appear in email header"
     message["To"] = ", ".join(receiver_email)
 
     html = """\
     <html>
-    <p><h4>Please update the short interest information!</h4><p>Hi Pepijn,</p><p>The automatic short interest tool has noticed that today's data has not yet been updated</p><p>Please update the short interest data file <strong>within 20 minutes </strong>in the folder&nbsp;<a href='K:\Trading\Short%20Interest Report' target='_blank'>here</a>&nbsp;</p><p>Thank you</p><p>Kempen Securities</p><p><span style='font-size: 6.0pt; text-transform: uppercase; letter-spacing: .5pt;'>Kempen&nbsp; N.V.&nbsp; Beethovenstraat 300&nbsp;&nbsp; 1077 WZ Amsterdam <br />Chamber of Commerce Amsterdam: 34186722</span><span style='font-size: 7.0pt; text-transform: uppercase; letter-spacing: .5pt;'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></p><p><span style='font-size: 7.0pt; text-transform: uppercase; letter-spacing: .5pt;'>THIS IS AN AUTOMATED MESSAGE, PLEASE DO NOT REPLY TO THIS EMAIL ADDRESS!</span></p><p></p>
+    <h4>Please update the short interest information!</h4> <p>Dear colleague,</p> <p>The automatic short interest tool has noticed that today's data has not yet been updated</p> <p>Please update the short interest data file <strong>within 20 minutes </strong>in the designated folder&nbsp;</p> <p>Thank you</p> <p><span style="font-size: 7.0pt; text-transform: uppercase; letter-spacing: .5pt;">THIS IS AN AUTOMATED MESSAGE, PLEASE DO NOT REPLY TO THIS EMAIL ADDRESS!</span></p> <p>&nbsp;</p>
     </html>
     """
     message.attach(MIMEText(html, "html"))
 
-    server = smtplib.SMTP('smtp.vlkintern.nl', 25)
-    server.connect("smtp.vlkintern.nl", 25)
+    server = smtplib.SMTP('smtp.servername.nl', 25)
+    server.connect("smtp.servername.nl", 25)
     server.sendmail(sender_email, receiver_email, message.as_string())
     server.quit()
     
@@ -279,22 +279,21 @@ if not datapoints.UpdateTimeStamp.iloc[0].date() == date.today():
     
     # If data still was not updated after 30 minutes, send email that data data was not updated and ask to update tomorrow
     else:        
-        sender_email = "olivier.kraaijeveld@kempen.com"
-        receiver_email = ['pepijn.kluin@kempen.com', 'olivier.kraaijeveld@kempen.com']
+        sender_email = "example@example.com"
+        receiver_email = ['example@example.com']
 
         message = MIMEMultipart("alternative")
         message["Subject"] = "ALERT: You have not updated the short interest data on time"
-        message["From"] = "Kempen Securities"
+        message["From"] = "Sender name you want to appear in email header"
         message["To"] = ", ".join(receiver_email)
 
         html = """\
         <html>
-        <p><h4>You have not updated today's short interest data, please update the data tomorrow!</h4><p>Hi Pepijn,</p><p>The automatic short interest tool has noticed that today's data still has not been updated.<p>The tool will run again tomorrow (except on weekend days) at 15.00, please make sure that you update the data on time!</p><p>If you have updated the data but still received this email, please contact Robbert van der Have (Quant Analyst) <a href='mailto:Robbert.vanderHave@kempen.com'>here</a>.</p><p>Thank you</p><p>Kempen Securities</p><p><span style='font-size: 6.0pt; text-transform: uppercase; letter-spacing: .5pt;'>Kempen&nbsp; N.V.&nbsp; Beethovenstraat 300&nbsp;&nbsp; 1077 WZ Amsterdam <br />Chamber of Commerce Amsterdam: 34186722</span><span style='font-size: 7.0pt; text-transform: uppercase; letter-spacing: .5pt;'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></p><p><span style='font-size: 7.0pt; text-transform: uppercase; letter-spacing: .5pt;'>THIS IS AN AUTOMATED MESSAGE, PLEASE DO NOT REPLY TO THIS EMAIL ADDRESS!</span></p>
-        </html>
+        <h4>You have not updated today's short interest data, please update the data tomorrow!</h4> <p>Dear colleague,</p> <p>The automatic short interest tool has noticed that today's data still has not been updated.</p> <p>The tool will run again tomorrow (except on weekend days) at 15.00, please make sure that you update the data on time!</p> <p>If you have updated the data but still received this email, please contact XXX.</p> <p>Thank you<span style="font-size: 7.0pt; text-transform: uppercase; letter-spacing: .5pt;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></p> <p><span style="font-size: 7.0pt; text-transform: uppercase; letter-spacing: .5pt;">THIS IS AN AUTOMATED MESSAGE, PLEASE DO NOT REPLY TO THIS EMAIL ADDRESS!</span></p>        </html>
         """
         message.attach(MIMEText(html, "html"))
 
-        server = smtplib.SMTP('smtp.vlkintern.nl', 25)
-        server.connect("smtp.vlkintern.nl", 25)
+        server = smtplib.SMTP('smtp.servername.nl', 25)
+        server.connect("smtp.servername.nl", 25)
         server.sendmail(sender_email, receiver_email, message.as_string())
         server.quit()
